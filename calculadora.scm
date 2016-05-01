77ea05f5f8f9187a705b76d196efdd7c36f7af0d
jarinlima <jarinlima@gmail.com>
2016-05-01 16:20:49 -0600

    PrimerComit

---
+++ calculadora.scm
@@ -0,0 +1,27 @@
+(define(filtro x)
+  (display "respuesta >> ")
+  (display x)
+  (main)
+  )
+
+(define (redirección x)
+(if (equal? x "quit")
+    (begin
+      (display "Saliendo.. ")
+      (newline)
+      (display "Gracias por utilizar nuestra calculadora")
+      )
+    (begin
+      (filtro x)
+         )
+    )
+  )
+  
+
+(define (main)
+  (newline)
+  (display "calculadora >> ")(define x (read-line))
+  (redirección x)
+  )
+(display "Calculadora de pepito")
+(main)
